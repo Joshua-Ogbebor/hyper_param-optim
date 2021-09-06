89a231e2739634bd2f2402d9dc967834860927fd
Josh <84150307+Joshua-Ogbebor@users.noreply.github.com>
2021-09-06 12:47:33 -0500

    Update fit.py
    Optimized the lines of code

--- train/fit.py
+++ train/fit.py
@@ -13,79 +13,56 @@ from pytorch_lightning.plugins import DDPPlugin
 from pytorch_lightning.loggers import TensorBoardLogger
 
 
-####### fit inception net using ASHA scheduler, or random search  #######
+def model_name (model_arch):
+   {'inc':inception_net.Googlenet_classifier,
+    'res':residual_net.Resnet_classifier}
+   return model[model_arch]
+####### fit net using ASHA scheduler, or random search  #######
 
-def train_fn_inc(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
-   
-    dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)
-    model = inception_net.Googlenet_Classifier(config,  dm.num_classes, data_dir)
-    metrics = {"loss": "val_loss", "acc": "val_accuracy"}
+def train_fn_asha(config, model_arch, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0, checkpoint_dir=None):
+   dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)
+   model = model_name(model_arch)(config,  dm.num_classes, data_dir)
+   metrics = {"loss": "val_loss", "acc": "val_accuracy"}
+   pl.seed_everything(42, workers=True)
+   trainer = pl.Trainer(
+      max_epochs=num_epochs,
+      gpus=num_gpus,
+      logger = TensorBoardLogger(save_dir=tune.get_trial_dir(), name="my_model"),
+      #log_every_n_steps=5000,
+      progress_bar_refresh_rate=0,
+      accelerator='ddp',
+      plugins=DDPPlugin(find_unused_parameters=False),
+      deterministic=True
+      callbacks=[TuneReportCallback(metrics, on="validation_end")])
 
-    trainer = pl.Trainer(
-        max_epochs=num_epochs,
-        gpus=num_gpus,
-        logger = TensorBoardLogger(save_dir=tune.get_trial_dir(), name="my_model"),
-        #log_every_n_steps=5000,
-        progress_bar_refresh_rate=0,
-        accelerator='ddp',
-        plugins=DDPPlugin(find_unused_parameters=False),
-        callbacks=[TuneReportCallback(metrics, on="validation_end")])
-
-    trainer.fit(model, dm)
-
-    
-###### fit resnet using ASHA scheduler, or random search ####
-
-def train_fn_res(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
-
-    dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)
-    model = residual_net.Resnet_Classifier(config, dm.num_classes, data_dir)
-    metrics = {"loss": "val_loss", "acc": "val_accuracy"}
-
-    trainer = pl.Trainer(
-        max_epochs=num_epochs,
-        gpus=num_gpus,
-        logger = TensorBoardLogger(save_dir=tune.get_trial_dir(), name="my_model"),
-        #log_every_n_steps=5000,
-        progress_bar_refresh_rate=0,
-        accelerator='ddp',
-        plugins=DDPPlugin(find_unused_parameters=False),
-        callbacks=[TuneReportCallback(metrics, on="validation_end")])
-
-    trainer.fit(model, dm)
-
+   trainer.fit(model, dm)
 
 ###### fit inception net using PBT scheduler ######
-
-def train_fn_inc_pbt(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
-
-    dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)    
-    metrics = {"loss": "val_loss", "acc": "val_accuracy"}
-    
-    kwargs = {
-        "max_epochs": num_epochs,
-        # If fractional GPUs passed in, convert to int.
-        "gpus": num_gpus,
-        "logger": TensorBoardLogger(
-            save_dir=tune.get_trial_dir(), name="", version="."),
-        "progress_bar_refresh_rate": 0,
-        "callbacks": [
-            TuneReportCheckpointCallback(
-                metrics=metrics,
-                filename="checkpoint",
-                on="validation_end")
+def train_fn_pbt(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
+   dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)    
+   metrics = {"loss": "val_loss", "acc": "val_accuracy"}
+   pl.seed_everything(42, workers=True)
+   kwargs = {
+      "max_epochs": num_epochs,
+      # If fractional GPUs passed in, convert to int.
+      "gpus": num_gpus,
+      "logger": TensorBoardLogger(
+          save_dir=tune.get_trial_dir(), name="", version="."),
+      "progress_bar_refresh_rate": 0,
+      "callbacks": [
+          TuneReportCheckpointCallback(
+              metrics=metrics,
+              filename="checkpoint",
+              on="validation_end")
         ]
-        "accelerator":'ddp',
-        "plugins":DDPPlugin(find_unused_parameters=False),
+      "accelerator":'ddp',
+      "plugins":DDPPlugin(find_unused_parameters=False),
+      "deterministic":True
     }
-
-    if checkpoint_dir:
-        kwargs["resume_from_checkpoint"] = os.path.join(
-            checkpoint_dir, "checkpoint")
-        
-    model = inception_net.Googlenet_Classifier(config=config, n_classes=dm.num_classes, data_dir=data_dir)
-    trainer = pl.Trainer(**kwargs)
-    trainer.fit(model,dm)
+   if checkpoint_dir:
+      kwargs["resume_from_checkpoint"] = os.path.join(checkpoint_dir, "checkpoint")
+   model = model_name(model_arch)(config,  dm.num_classes, data_dir)   trainer = pl.Trainer(**kwargs)
+   trainer.fit(model,dm)
     
     
     
@@ -93,32 +70,30 @@ def train_fn_inc_pbt(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num
 ###### fit resnet using PBT scheduler ########    
 
 def train_fn_res_pbt(config, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
-
-    dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)
-    metrics = {"loss": "val_loss", "acc": "val_accuracy"}
-    
-    kwargs = {
-        "max_epochs": num_epochs,
-        # If fractional GPUs passed in, convert to int.
-        "gpus":num_gpus,
-        "logger": TensorBoardLogger(
-            save_dir=tune.get_trial_dir(), name="", version="."),
-        "progress_bar_refresh_rate": 0,
-        "callbacks": [
-            TuneReportCheckpointCallback(
-                metrics=metrics,
-                filename="checkpoint",
-                on="validation_end")
+   dm = datamodule.ImgData(num_workers=0, batch_size=config["batch_size"], data_dir=data_dir)    
+   metrics = {"loss": "val_loss", "acc": "val_accuracy"}
+   pl.seed_everything(42, workers=True)
+   kwargs = {
+      "max_epochs": num_epochs,
+      # If fractional GPUs passed in, convert to int.
+      "gpus": num_gpus,
+      "logger": TensorBoardLogger(
+          save_dir=tune.get_trial_dir(), name="", version="."),
+      "progress_bar_refresh_rate": 0,
+      "callbacks": [
+          TuneReportCheckpointCallback(
+              metrics=metrics,
+              filename="checkpoint",
+              on="validation_end")
         ]
-        "accelerator":'ddp',
-        "plugins":DDPPlugin(find_unused_parameters=False),
+      "accelerator":'ddp',
+      "plugins":DDPPlugin(find_unused_parameters=False),
+      "deterministic":True
     }
 
-    if checkpoint_dir:
-        kwargs["resume_from_checkpoint"] = os.path.join(
-            checkpoint_dir, "checkpoint")
-        
-    model = residual_net.Resnet_Classifier(config, dm.num_classes, data_dir)
-    trainer = pl.Trainer(**kwargs)
-    trainer.fit(model,dm)
+   if checkpoint_dir:
+      kwargs["resume_from_checkpoint"] = os.path.join(checkpoint_dir, "checkpoint")    
+   model = residual_net.Resnet_Classifier(config, dm.num_classes, data_dir)
+   trainer = pl.Trainer(**kwargs)
+   trainer.fit(model,dm)
 

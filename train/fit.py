bf112a31fbc87f904fa3bc2675975b3d17b81a9a
Josh <84150307+Joshua-Ogbebor@users.noreply.github.com>
2022-04-04 20:26:13 -0500

    Update fit.py

--- train/fit.py
+++ train/fit.py
@@ -1,42 +1,39 @@
 from ray.tune.integration.pytorch_lightning import TuneReportCallback, TuneReportCheckpointCallback
+#import comet_ml
 import torch, os, sys
 import tempfile
 from ray import tune
 import pytorch_lightning as pl
 sys.path.append("..")
-<<<<<<< HEAD
-from model import residual_net 
-from model import inception_net
-from model import vgg_net
-from model import alex_net
-=======
 from model import residual_net, inception_net, alex_net
->>>>>>> 43e9b2048f02a4a427d7136f37b38b0809017528
 from data import datamodule
 from pytorch_lightning.plugins import DDPPlugin
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import CometLogger
+from private.comet_key import key
 
 
 def model_name (model_arch):
    return {'inc':inception_net.Googlenet_Classifier,
-    'res':residual_net.Resnet_Classifier,
+    'res':residual_net.resNet_custom,
     'alex': alex_net.AlexNet ,
-    'vgg': vgg_net.VGG
+    #'vgg': vgg_net.VGG
    }[model_arch]
 ####### fit net using ASHA scheduler, or random search  #######
 
 def train_fn(config, model_arch, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0, checkpoint_dir=None):
-   dm = datamodule.ImgData(num_workers=8, batch_size=config["batch_size"],data_dir=data_dir)
+   dm = datamodule.ImgData(num_workers=1, batch_size=config["batch_size"],data_dir=data_dir)
    model = model_name(model_arch)(config,  dm.num_classes)
-   metrics = {"loss": "val_loss", "acc": "val_accuracy"}
+   metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
    pl.seed_everything(42, workers=True)
+   comet_logger=CometLogger(api_key = key(), experiment_name =model_arch, project_name = "deep-tuner-stage-2")
    trainer = pl.Trainer(
       max_epochs=num_epochs,
       gpus=num_gpus,
-      logger = TensorBoardLogger(save_dir=tune.get_trial_dir(), name="my_model"),
+      logger = [comet_logger,TensorBoardLogger(save_dir=tune.get_trial_dir(), name="my_model")],
       #log_every_n_steps=5000,
       progress_bar_refresh_rate=0,
-      accelerator='ddp',
+      #accelerator='ddp',
       plugins=DDPPlugin(find_unused_parameters=False),
       deterministic=True,
       callbacks=[TuneReportCallback(metrics, on="validation_end")])
@@ -45,7 +42,7 @@ def train_fn(config, model_arch, data_dir=os.path.join(os.getcwd(), "Dataset") ,
 
 ###### fit inception net using PBT scheduler ######
 def train_fn_pbt(config,model_arch, data_dir=os.path.join(os.getcwd(), "Dataset") , num_epochs=60, num_gpus=0,  checkpoint_dir=None):
-   dm = datamodule.ImgData(num_workers=8, batch_size=config["batch_size"], data_dir=data_dir)    
+   dm = datamodule.ImgData(num_workers=8, batch_size=config["batch_size"], data_dir=data_dir)
    metrics = {"loss": "val_loss", "acc": "val_accuracy"}
    pl.seed_everything(42, workers=True)
    kwargs = {
@@ -67,11 +64,12 @@ def train_fn_pbt(config,model_arch, data_dir=os.path.join(os.getcwd(), "Dataset"
     }
    if checkpoint_dir:
       kwargs["resume_from_checkpoint"] = os.path.join(checkpoint_dir, "checkpoint")
-   model = model_name(model_arch)(config,  dm.num_classes, data_dir)   
+   model = model_name(model_arch)(config,  dm.num_classes, data_dir)
    trainer = pl.Trainer(**kwargs)
    trainer.fit(model,dm)
-    
-    
-    
-    
-###### fit resnet using PBT scheduler ########    
+
+
+
+
+#####:wq
+# fit resnet using PBT scheduler ########

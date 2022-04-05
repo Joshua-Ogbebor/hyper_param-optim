a171b2dff078137a52536e685a06e3f226e7ea6f
Josh <84150307+Joshua-Ogbebor@users.noreply.github.com>
2022-04-04 20:34:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 from ray.tune.schedulers import ASHAScheduler, PopulationBasedTraining
 import torchmetrics
 from ray import tune
+from config import *
 
 
 def main (num_samples=40, num_epochs=50, folder="Dataset", arch='inc',optim=None):
@@ -66,75 +67,6 @@ def main (num_samples=40, num_epochs=50, folder="Dataset", arch='inc',optim=None
 
 def config_dict (arch,optim):
      ######## Config for architectures ############
-    
-    config_inc_pbt = {
-        "lr": 1e-4,
-        "mm": 0.6,
-        "dp":0,
-        "wD": 0.000008,
-        "depth":tune.choice([1,2,3]),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size": 64,
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2": tune.choice([0.999]),
-        "eps":tune.loguniform(1e-08 ,1e-04),
-        "rho":tune.choice([0.9])
-
-    }
-    config_alex_pbt = {
-        "lr": 1e-4,
-        "mm": 0.6,
-        "dp":0,
-        "wD": 0.000008,
-        #"depth":tune.choice([1,2,3]),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size": 64,
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2": 0.999,
-        "eps":tune.loguniform(1e-08 ,1e-04),
-        "rho":0.9
-    }
-    config_vgg_pbt = {
-        "lr": 1e-4,
-        "mm": 0.6,
-        "dp":0,
-        "wD": 0.000008,
-        "vgg_config":tune.choice(['A','B','D','E']),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size": 64,
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2": 0.999,
-        "eps":tune.loguniform(1e-08 ,1e-04),
-        "rho":0.9,
-        "batch_norm": tune.choice([True,False]),
-
-    }
-
-    config_res_pbt = {
-        "lr": 1e-4,
-        "mm": 0.6,
-        "dp":0,
-        "wD": 0.000008,
-        "bloc_1":tune.choice([64,128,256,512]),
-        "bloc_2":tune.choice([64,128,256,512]),
-        "bloc_3":tune.choice([64,128,256,512]),
-        "bloc_4":tune.choice([64,128,256,512]),
-        "depth_1":tune.choice([1,2]),
-        "depth_2":tune.choice([1,2,0]),
-        "depth_3":tune.choice([1,2,0]),
-        "depth_4":0,#tune.choice([1,2,0]),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size": 64,
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2": 0.999,
-        "eps":tune.loguniform(1e-08 ,1e-04),
-        "rho":0.9
-    }
-
     if arch =='inc':
        config=config_inc_pbt if optim=="pbt" else config_inc
     elif arch =='res':
